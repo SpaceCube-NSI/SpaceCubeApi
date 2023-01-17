74a3bf33b387c1fd7dafb238b089276a6a2f7c5c
Mathis_Bruel <70230064+MathisBruel@users.noreply.github.com>
2023-01-17 09:49:39 +0100

    update

--- src/main/java/fr/Mathis_Bruel/spacecube/spacecubeapi/api/games/Stats.java
+++ src/main/java/fr/Mathis_Bruel/spacecube/spacecubeapi/api/games/Stats.java
@@ -1,43 +1,214 @@
 package fr.Mathis_Bruel.spacecube.spacecubeapi.api.games;
 
+import fr.Mathis_Bruel.spacecube.spacecubeapi.DataBase.Connection;
+import fr.Mathis_Bruel.spacecube.spacecubeapi.Main;
+import fr.Mathis_Bruel.spacecube.spacecubeapi.api.Ranks.Ranks;
+import jdk.nashorn.internal.objects.annotations.Getter;
+import jdk.nashorn.internal.objects.annotations.Setter;
+
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.HashMap;
+
 public class Stats {
 
-  @Getter private Game game;
-  @Getter @Setter private int kills;
-  @Getter @Setter private int death;
-  @Getter @Setter private float ratioKillsDeath;
-  @Getter @Setter private int games;
-  @Getter @Setter private int wins;
-  @Getter @Setter private int loses;
-  @Getter @Setter private float ratioWinsLoses;
-  @Getter @Setter private HashMapo<Object, Object> divers;
-  
-
-  public Stats(){
-    
-  }
-
-  public void addKills(int kills){
-    this.kills += kills;
-  }
-  public void removeKills(int kills){
-    this.kills -= kills;
-  }
-
-  public void addGames(int gamesPlayed){
-    this.games += gamesPlayed;
-  }
-  public void removeGames(int gamesPlayed){
-    this.games -= gamesPlayed;
-  }
-
-  public void addWins(int wins){
-    this.wins += wins;
-  }
-  public void removeWins(int wins){
-    this.wins -= wins;
-  }
+    private Games game;
+    private int kills;
+    private int death;
+    private float ratioKillsDeath;
+    private int games;
+    private int wins;
+    private int loses;
+    private float ratioWinsLoses;
+    private HashMap<Object, Object> divers;
 
 
-  
+    public Stats() {
+
+    }
+
+    public int getKills() {
+        return this.kills;
+    }
+
+    public int getDeath() {
+        return this.death;
+    }
+
+    public float getRatioKillsDeath() {
+        return this.ratioKillsDeath;
+    }
+
+    public int getGames() {
+        return this.games;
+    }
+
+    public int getWins() {
+        return this.wins;
+    }
+
+    public int getLoses() {
+        return this.loses;
+    }
+
+    public float getRatioWinsLoses() {
+        return this.ratioWinsLoses;
+    }
+
+    public HashMap<Object, Object> getDivers() {
+        return this.divers;
+    }
+
+    public void setKills(int kills) {
+        this.kills = kills;
+    }
+
+    public void setDeath(int death) {
+        this.death = death;
+    }
+
+    public void setRatioKillsDeath(float ratioKillsDeath) {
+        this.ratioKillsDeath = ratioKillsDeath;
+    }
+
+    public void setGames(int games) {
+        this.games = games;
+    }
+
+    public void setWins(int wins) {
+        this.wins = wins;
+    }
+
+    public void setLoses(int loses) {
+        this.loses = loses;
+    }
+
+    public void setRatioWinsLoses(float ratioWinsLoses) {
+        this.ratioWinsLoses = ratioWinsLoses;
+    }
+
+    public void setDivers(HashMap<Object, Object> divers) {
+        this.divers = divers;
+    }
+
+    public void setGame(Games game) {
+        this.game = game;
+    }
+
+    public Games getGame() {
+        return this.game;
+    }
+
+    public void addKills(int kills) {
+        this.kills += kills;
+    }
+
+    public void removeKills(int kills) {
+        this.kills -= kills;
+    }
+
+    public void addGames(int gamesPlayed) {
+        this.games += gamesPlayed;
+    }
+
+    public void removeGames(int gamesPlayed) {
+        this.games -= gamesPlayed;
+    }
+
+    public void addWins(int wins) {
+        this.wins += wins;
+    }
+
+    public void removeWins(int wins) {
+        this.wins -= wins;
+    }
+
+    public void addLoses(int loses) {
+        this.loses += loses;
+    }
+
+    public void removeLoses(int loses) {
+        this.loses -= loses;
+    }
+
+    public void addDeath(int death) {
+        this.death += death;
+    }
+
+    public void removeDeath(int death) {
+        this.death -= death;
+    }
+
+    public void addDivers(Object key, Object value) {
+        this.divers.put(key, value);
+    }
+
+    public void removeDivers(Object key) {
+        this.divers.remove(key);
+    }
+
+    public void updateRatioKillsDeath() {
+        this.ratioKillsDeath = (float) this.kills / (float) this.death;
+    }
+
+    public void updateRatioWinsLoses() {
+        this.ratioWinsLoses = (float) this.wins / (float) this.loses;
+    }
+
+    public void updateRatio() {
+        this.ratioKillsDeath = (float) this.kills / (float) this.death;
+        this.ratioWinsLoses = (float) this.wins / (float) this.loses;
+    }
+
+
+    public void reset() {
+        this.kills = 0;
+        this.death = 0;
+        this.ratioKillsDeath = 0;
+        this.games = 0;
+        this.wins = 0;
+        this.loses = 0;
+        this.ratioWinsLoses = 0;
+        this.divers = new HashMap<>();
+    }
+
+
+    public void updateStats(){
+        // update stats in db
+
+        final Connection connection2 = Main.getDbManageur().getConnection();
+        try {
+            final java.sql.Connection connection = connection2.getConnection();
+            String query = "SELECT UUID, Kills, Death FROM Stats WHERE UUID=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(query);
+            preparedStatement.setString(1, player.getUniqueId().toString());
+            final ResultSet resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+
+            } else {
+                // Player doesn't exist
+                PreparedStatement preparedStatement2 = connection.prepareStatement("INSERT INTO `Players`(`UUID`, `Money`, `Rank`, `Permissions`) VALUES (?,?,?,?)");
+                preparedStatement2.setString(1, player.getUniqueId().toString());
+                preparedStatement2.setInt(2, 0);
+                preparedStatement2.setString(3, Ranks.getDefaultRank().getName());
+                preparedStatement2.setString(4, "");
+                preparedStatement2.executeUpdate();
+                preparedStatement2.close();
+            }
+            resultSet.close();
+            connection.close();
+            preparedStatement.close();
+        } catch (SQLException e) {
+            System.out.println(e);
+            return null;
+        }
+        this.uuid = player.getUniqueId();
+        this.money = 0;
+        this.permissions = null;
+        Main.getApi().scp.put(player.getUniqueId(), this);
+        return this;
+    }
+
+
 }
--- src/main/java/fr/Mathis_Bruel/spacecube/spacecubeapi/api/games/games.java
+++ src/main/java/fr/Mathis_Bruel/spacecube/spacecubeapi/api/games/games.java
@@ -1,8 +1,33 @@
 package fr.Mathis_Bruel.spacecube.spacecubeapi.api.games;
 
-public enum games {
+public enum Games {
     BedWars,
     Practice,
     DeathRun,
-    FallGuys
+    Thimble;
+
+    private String Name;
+    private String Prefix;
+    private String ChatColor;
+    private Stats Stats;
+
+    public String getName(){
+        return this.Name;
+    }
+
+    public String getPrefix(){
+        return this.Prefix;
+    }
+
+    public String getChatColor(){
+        return this.ChatColor;
+    }
+
+    public Stats getStats(){
+        return this.Stats;
+    }
+
+
 }
+
+
